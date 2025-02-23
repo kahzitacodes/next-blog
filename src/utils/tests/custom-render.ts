c6fff7c8bf5054d6131e1b65737be60dbd5ed3d5
Karine Pereira <karinesp89@gmail.com>
2025-02-23 21:21:24 +0000

    feat: add custom render for tests

---
+++ src/utils/tests/custom-render.ts
@@ -0,0 +1,9 @@
+import { render, RenderOptions } from '@testing-library/react'
+
+const customRender = (
+  ui: React.ReactElement,
+  options: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
