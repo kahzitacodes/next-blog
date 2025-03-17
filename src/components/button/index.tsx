2dbbb4df663c7c1dac8a1d03c483ba27b0dc6a6b
Karine Pereira <karinesp89@gmail.com>
2025-03-17 22:01:01 +0000

    chore: config tailwind

---
+++ src/components/button/index.tsx
@@ -0,0 +1,3 @@
+import * as S from './styles'
+
+export const Button = () => <S.Container>Click</S.Container>
---
+++ src/components/button/styles.ts
@@ -0,0 +1,13 @@
+import tw from 'tailwind-styled-components'
+
+export const Container = tw.button`
+  bg-red-400
+  text-white
+  font-medium
+  h-[40px]
+  px-4
+  rounded-md
+  hover:bg-red-500
+  duration-200
+  transition-colors
+`
