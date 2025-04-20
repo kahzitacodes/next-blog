73f32ccac2b4a30fa3e6cfb5926425db7a57011b
Karine Pereira <karinesp89@gmail.com>
2025-04-20 16:08:53 +0100

    fix: add fix to mdx tests

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom'
+const React = require('react')
+
+jest.mock('next-contentlayer/hooks', () => ({
+  useMDXComponent: () => () => React.createElement('div', null, 'Mocked MDX')
+}))
