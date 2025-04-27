af40c6c7c58254ce4dd3e581b89b0c212140c3ce
Karine Pereira <karinesp89@gmail.com>
2025-04-27 12:46:51 +0100

    chore: pwa config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-require-imports */
 const { withContentlayer } = require('next-contentlayer')
+const withPWAInit = require('next-pwa')
+const withPlugins = require('next-compose-plugins')
+
+const isDev = process.env.NODE_ENV !== 'production'
+const withPWA = withPWAInit({
+  dest: 'public',
+  exclude: [
+    ({ asset }) => {
+      if (
+        asset.name.startsWith('server/') ||
+        asset.name.match(
+          /^((app-|ˆ)build-manifest\.json|react-loadable-manifest\.json)$/
+        )
+      ) {
+        return true
+      }
+      if (isDev && !asset.name.startsWith('static/runtime/')) {
+        return true
+      }
+      return false
+    }
+  ]
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withPlugins([withPWA, withContentlayer], nextConfig)
